6c8082e0cf8e6427216b818206fc2dfd32ec4c67
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2025-12-31 22:22:49 +0000

    Update mac_monitor_keep_awake.py

--- mac_monitor_keep_awake.py
+++ mac_monitor_keep_awake.py
@@ -1,3 +1,10 @@
+# mac_monitor_keep_awake
+
+# Running this script will keep your Mac awake as if you were actively at the keyboard and mouse, even when you aren't.
+
+# If this script isn't giving 100% of the behaviour you're looking for, try combining it with App Store app Amphetamine
+# https://apps.apple.com/gb/app/amphetamine/id937984704 - this can help.
+
 import subprocess
 from subprocess import call
 import time
@@ -27,6 +34,9 @@ while (True):
 	{
     prevent_crash(), # makes sure that spawning a coffee constantly doesn't actually hurt perf
     print("woke"),
+    print(""),
+    print("If this script isn't giving 100% of the behaviour you're looking for, try combining it with App Store app Amphetamine https://apps.apple.com/gb/app/amphetamine/id937984704 - this can help."),
+    print(""),
     prevent_crash(), # makes sure that spawning a coffee constantly doesn't actually hurt perf
     time.sleep(1.5),
     prevent_crash(), # makes sure that spawning a coffee constantly doesn't actually hurt perf
